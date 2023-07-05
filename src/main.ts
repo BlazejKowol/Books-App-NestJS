08ea21396a772f5ac58b8a40f899938488a5dd2b
Błażej Kowol <kowol.blazej@gmail.com>
2023-07-05 22:02:54 +0200

    Initial commit with an app structure

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { PrismaService } from './shared/services/prisma.service';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.setGlobalPrefix('api');
+
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
+
+  await app.listen(3000);
+  console.log('Server is running on port 3000');
+}
+bootstrap();
