d14779ccbf9e49d5047bc6d6cb18b3e56b85b489
Błażej Kowol <kowol.blazej@gmail.com>
2023-07-07 20:12:26 +0200

    add Books module

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,7 +1,49 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
 import { BooksService } from './books.service';
+import { ParseUUIDPipe } from '@nestjs/common';
+import { NotFoundException } from '@nestjs/common';
+import { CreateBookDTO } from './dtos/create-book.dto';
+import { UpdateBookDTO } from './dtos/update-book.dto';
 
 @Controller('books')
 export class BooksController {
-  constructor(private bookService: BooksService) {}
+  constructor(private booksService: BooksService) {}
+
+  @Get('/')
+  getAll(): any {
+    return this.booksService.getAll();
+  }
+
+  @Get('/:id')
+  public async getById(@Param('id', new ParseUUIDPipe()) id: string) {
+    const book = await this.booksService.getById(id);
+    if (!book) throw new NotFoundException('404 not found');
+    return book;
+  }
+
+  @Post('/')
+  create(@Body() authorData: CreateBookDTO) {
+    return this.booksService.create(authorData);
+  }
+
+  @Put('/:id')
+  async update(
+    @Param('id', new ParseUUIDPipe()) id: string,
+    @Body() authorData: UpdateBookDTO,
+  ) {
+    if (!(await this.booksService.getById(id)))
+      throw new NotFoundException('400 Bad Request');
+
+    await this.booksService.updateById(id, authorData);
+    return { success: true };
+  }
+
+  @Delete('/:id')
+  async delete(@Param('id', new ParseUUIDPipe()) id: string) {
+    if (!(await this.booksService.getById(id)))
+      throw new NotFoundException('404 not found');
+
+    await this.booksService.deleteById(id);
+    return { success: true };
+  }
 }
--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,8 +1,68 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/shared/services/prisma.service';
 import { Book } from '@prisma/client';
+import { ConflictException } from '@nestjs/common';
 
 @Injectable()
 export class BooksService {
   constructor(private prismaService: PrismaService) {}
+
+  public getAll(): Promise<Book[]> {
+    return this.prismaService.book.findMany({ include: { author: true } });
+  }
+
+  public getById(id: Book['id']): Promise<Book | null> {
+    return this.prismaService.book.findUnique({
+      where: { id },
+      include: { author: true },
+    });
+  }
+
+  public async create(
+    booksData: Omit<Book, 'id' | 'createdAt' | 'updatedAt'>,
+  ): Promise<Book> {
+    const { authorId, ...otherData } = booksData;
+    try {
+      return await this.prismaService.book.create({
+        data: {
+          ...otherData,
+          author: {
+            connect: { id: authorId },
+          },
+        },
+      });
+    } catch (err) {
+      if (err.code === 'P2002')
+        throw new ConflictException('Title is already taken');
+      throw err;
+    }
+  }
+
+  public async updateById(
+    id: Book['id'],
+    booksData: Omit<Book, 'id' | 'createdAt' | 'updatedAt'>,
+  ): Promise<Book> {
+    const { authorId, ...otherData } = booksData;
+    try {
+      return await this.prismaService.book.update({
+        where: { id },
+        data: {
+          ...otherData,
+          author: {
+            connect: { id: authorId },
+          },
+        },
+      });
+    } catch (err) {
+      if (err.code === 'P2002')
+        throw new ConflictException('Title is already taken');
+      throw err;
+    }
+  }
+
+  public deleteById(id: Book['id']): Promise<Book> {
+    return this.prismaService.book.delete({
+      where: { id },
+    });
+  }
 }
